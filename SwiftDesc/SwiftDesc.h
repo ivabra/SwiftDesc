0170976271e3ecb4a48b80925f29c337c1e47233
Ivan Brazhnikov <samsungpc239@gmail.com>
2018-06-14 12:06:08 +0200

    Initial Commit

---
+++ SwiftDesc/SwiftDesc.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDesc.h
+//  SwiftDesc
+//
+//  Created by Ivan Brazhnikov on 14/06/2018.
+//  Copyright © 2018 Ivan Brazhnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDesc.
+FOUNDATION_EXPORT double SwiftDescVersionNumber;
+
+//! Project version string for SwiftDesc.
+FOUNDATION_EXPORT const unsigned char SwiftDescVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDesc/PublicHeader.h>
+
+
